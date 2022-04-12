9142967c1c26f3fb4d7dcd394b4cd921d163e158
Shubham chugh <chugh.shubham12@gmail.com>
2022-04-12 16:49:32 +0530

    improve

--- install.sh
+++ install.sh
@@ -1,32 +1,36 @@
 
-echo "disabling firewall";
+echo "===========disabling firewall=============";
 sudo ufw disable
-echo "firewall disabled";
+echo "===========firewall disabled==============";
 
 
 echo  "installing Node Js Version:  v16.14.2";
-$("curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh)
-$("curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash")
+$(curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh)
+$(curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh)  | bash
 source ~/.bashrc
 nvm install v16.14.2
-echo "Node js installed";
-echo "Chromium dependeancy installing"
+echo "============Node js installed===========";
+node -v
+echo "============Chromium dependeancy installing============"
 sudo apt-get update
 sudo apt-get install gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
 sudo apt-get install -y libgbm-dev
 sudo apt-get install libglib2.0-0
 
-echo "installing Pm2"
+echo "============installing Pm2============"
 npm install pm2 -g
 
 
-echo "Chaning direactory to node Project /home/NodeJS-API/"
+echo "============Chaning direactory to node Project /home/NodeJS-API/============"
 cd /home/NodeJS-API/
 
-echo "starting project"
+echo "============starting project============"
 pm2 start googleBingScraper.js -i max --watch
 
-echo "setting up Nodejs server restart after reboot"
+echo "============re-starting project============"
+pm2 restart googleBingScraper.js -i max --watch
+
+echo "============setting up Nodejs server restart after reboot============"
 pm2 startup
 pm2 save
 
